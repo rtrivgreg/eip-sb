33eae5e44f33cea33d3cfeb922cff4811b1f185e
rtrivgreg <rtrivgreg@yahoo.com>
2026-04-26 09:11:45 -0400

    Add outputs for VPC profile and database subnets

--- poc/outputs.tf
+++ poc/outputs.tf
@@ -2,10 +2,18 @@ output "poc_vpc_id" {
   value = module.poc_vpc.vpc_id
 }
 
+output "poc_vpc_profile" {
+  value = var.vpc_profile
+}
+
 output "poc_public_subnets" {
   value = module.poc_vpc.public_subnets
 }
 
 output "poc_private_subnets" {
   value = module.poc_vpc.private_subnets
+}
+
+output "poc_database_subnets" {
+  value = module.poc_vpc.database_subnets
 }
