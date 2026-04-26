980778d37f0852ec9520ecee5dbed0558b6f5311
rtrivgreg <rtrivgreg@yahoo.com>
2026-04-26 09:09:13 -0400

    Refactor VPC module to use local variables

--- poc/vpc.tf
+++ poc/vpc.tf
@@ -1,24 +1,28 @@
 module "poc_vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.0"  # keep it explicit, 5.x is stable and widely used[web:382][web:400]
+  version = "~> 5.0"
 
-  name = "rg-poc-vpc"
-  cidr = "10.10.0.0/16"
+  name = local.profile.name
+  cidr = local.profile.cidr
 
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  public_subnets  = ["10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24"]
-  private_subnets = ["10.10.11.0/24", "10.10.12.0/24", "10.10.13.0/24"]
+  azs = local.azs
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  public_subnets   = local.public_subnets
+  private_subnets  = local.private_subnets
+  database_subnets = local.database_subnets
 
-  # Important: let AWS keep the default NACL & default SG as-is for this POC
-  manage_default_network_acl   = false  # disables aws_default_network_acl.this inside the module[web:413][web:415]
-  manage_default_security_group = false # optional, avoids recreating default SG if you don't care to manage it[web:417][web:415]
+  enable_nat_gateway = local.profile.enable_nat_gateway
+  single_nat_gateway = local.profile.single_nat_gateway
 
-  tags = {
-    Project     = "vpce-poc"
-    Environment = "poc"
-    ManagedBy   = "terraform"
-  }
+  enable_dns_support   = local.profile.enable_dns_support
+  enable_dns_hostnames = local.profile.enable_dns_hostnames
+
+  map_public_ip_on_launch = local.profile.map_public_ip_on_launch
+
+  create_database_subnet_group = local.profile.create_database_subnet_group
+
+  manage_default_network_acl    = false
+  manage_default_security_group = false
+
+  tags = local.common_tags
 }
