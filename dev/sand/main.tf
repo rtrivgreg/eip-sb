4ce183c95559644ffc5d532bfebca39bf6686521
Raymond Gregoire <rtrivgreg@yahoo.com>
2026-08-09 23:46:26 +0000

    vpc_security_group_ids = ["sg-02c3be99d2bc862ca"]

--- dev/sand/main.tf
+++ dev/sand/main.tf
@@ -3,7 +3,8 @@ resource "aws_instance" "example" {
   ami                   = "ami-0b6c6ebed2801a5cb"  # needs for baseline
   instance_type          = "t3.nano"
   subnet_id             = "subnet-0e2edd5fa0e3907f0"
-  security_groups       = ["sg-02c3be99d2bc862ca"]
+  #security_groups       = ["sg-02c3be99d2bc862ca"]
+  vpc_security_group_ids = ["sg-02c3be99d2bc862ca"]
   key_name              = "TF3"
 
   #vpcsecuritygroupids = var.securitygroups  
